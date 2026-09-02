daa052be8dd7cfb600260ecc54c41ddabd69a0f7
Sadick Mumin <102040809+sadick22@users.noreply.github.com>
2026-09-02 10:11:16 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import CoreoProductionHub from "./CoreoProductionHub.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <CoreoProductionHub />
+);
